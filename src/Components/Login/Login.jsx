e9d528ae3a912f4527ae59166cb6e34782d2c8ae
Mohammed-A7med <mohamedahmedkhalaf68@gmail.com>
2025-07-23 12:35:39 +0300

    fix(login): set default email and password values for login form

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -81,7 +81,7 @@ export default function Login(props) {
         className="form-control my-2"
         type="password"
         name="password"
-        defaultValue={user.email}
+        defaultValue={user.password}
       />
       <div className="btn-register d-flex justify-content-end my-4">
         <button className="btn btn-info" type="submit" disabled={isLoading}>
