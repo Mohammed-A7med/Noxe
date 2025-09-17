2b7e77cee310c5abf76a721a2f7e8f7fa5457b6a
Mohammed-A7med <mohamedahmedkhalaf68@gmail.com>
2025-09-17 23:39:39 +0300

    fix(firebase): remove process.env usage and use direct firebase config to resolve invalid API key error

--- src/Components/Firebase/firebase.js
+++ src/Components/Firebase/firebase.js
@@ -14,13 +14,13 @@ import { getAuth } from "firebase/auth";
 
 // for React App
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAknAuFBda9fUao4tkiPkaYNajIt7LaoM8",
+  authDomain: "noxe-fdc7b.firebaseapp.com",
+  projectId: "noxe-fdc7b",
+  storageBucket: "noxe-fdc7b.appspot.com",
+  messagingSenderId: "6162628501",
+  appId: "1:6162628501:web:7d666defe38d96f556409c",
+  measurementId: "G-XZ77BTNVWX",
 };
 
 const app = initializeApp(firebaseConfig);
